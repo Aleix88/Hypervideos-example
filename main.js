b28f143bd8b41f4a7006a6223ee45a0f1ea988f9
Aleix88 <aleixdiazbaggerman@gmail.com>
2021-06-14 11:06:39 +0200

    Example updated

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const youtubeConfig = {
             "plugin": {
                 "name": "SimpleLabel",
                 "config": {
-                    "text": "Hi! I'm a label! <a href=\"https://www.google.es\"> Also anchor links</a>"
+                    "text": "Hi! I'm a label!"
                 }
             }
         },
@@ -106,7 +106,7 @@ const youtubeConfig = {
 };
 
 const videoConfig = {
-    "src":"./assets/video.mp4",
+    "src":"./assets/solarsystem.mp4",
     "id":"hypervideo_file",
     "type": Hypervideo.VIDEO_TYPE,
     "videoTitle": "Hypervideo from a file",
@@ -117,18 +117,29 @@ const videoConfig = {
     "tags": [
         {
             "position": {
-                "x": 50,
-                "y": 20
+                "x": 40,
+                "y": 55
             },
             "timeConfig": {
-                "timestamp": 1,
-                "duration": 10
+                "timestamp": 28,
+                "duration": 5
             },
-            "color": "#fc9403",
+            "color": "#f54e42",
             "plugin": {
-                "name": "SimpleLabel",
+                "name": "Card",
                 "config": {
-                    "text": "Hi! I'm a label! <a href=\"https://www.google.es\"> Also anchor links</a>"
+                    "title": "The milky way",
+                    "description": "The Milky Way[a] is the galaxy that includes our Solar System, with the name describing the galaxy's appearance from Earth: a hazy band of light seen in the night sky formed from stars that cannot be individually distinguished by the naked eye. The term Milky Way is a translation of the Latin via lactea, from the Greek γαλακτικός κύκλος (galaktikos kýklos, milky circle).[19][20][21] From Earth, the Milky Way appears as a band because its disk-shaped structure is viewed from within. Galileo Galilei first resolved the band of light into individual stars with his telescope in 1610.",
+                    "imageSrc": "./assets/milkyway.jpg",
+                    "backgroundColor": "#F2D5A0",
+                    "titleColor": "rgb(138 104 43)",
+                    "descriptionColor": "rgb(138 104 43)",
+                    "button": {
+                      "title": "Read Post",
+                      "href": "https://en.wikipedia.org/wiki/Milky_Way", 
+                      "backgroundColor": "rgb(138 104 43)",
+                      "textColor": "white"
+                    }
                 }
             }
         },
@@ -138,48 +149,20 @@ const videoConfig = {
                 "y": 60
             },
             "timeConfig": {
-                "timestamp": 2,
-                "duration": 10
+                "timestamp": 35,
+                "duration": 5
             },
             "color": "#fc9403",
             "plugin": {
                 "name": "Questionary",
                 "config": {
-                    "title": "Is this the most awesome js library?",
+                    "title": "What is the mily way?",
                     "subtitle": "Choose the correct answer",
-                    "correctAnswer": "Yeah! This is the most awesome library!",
+                    "correctAnswer": "The Milky Way is the galaxy that includes our Solar System",
                     "wrongAnswers": [
-                      "This is a random answer!",
-                      "Hi! I'm a random answer too!"
+                      "The Milky Way is some kind of food that is made with milk.",
+                      "A galaxy that is located in the center of the universe."
                     ]
-                }
-            }
-        },
-        {
-            "position": {
-                "x": 40,
-                "y": 55
-            },
-            "timeConfig": {
-                "timestamp": 4,
-                "duration": 10
-            },
-            "color": "#ffffff",
-            "plugin": {
-                "name": "Card",
-                "config": {
-                    "title": "Landscape photography",
-                    "description": "When most people start out taking landscape photos, they think they need to get a wide angle lens in order to capture the whole landscape. When I bought my first DSLR, I was one of those people. I made sure I had a wide angle lens because I knew I mostly wanted to photograph landscapes from my adventures, and that’s what everyone told me I required to get the job done.",
-                    "imageSrc": "./assets/nature.jpg",
-                    "backgroundColor": "#F2D5A0",
-                    "titleColor": "rgb(138 104 43)",
-                    "descriptionColor": "rgb(138 104 43)",
-                    "button": {
-                      "title": "Read Post",
-                      "href": "https://www.google.es", 
-                      "backgroundColor": "rgb(138 104 43)",
-                      "textColor": "white"
-                    }
                 }
             }
         },
@@ -189,21 +172,21 @@ const videoConfig = {
                 "y": 90
             },
             "timeConfig": {
-                "timestamp": 10,
-                "duration": 2
+                "timestamp": 49,
+                "duration": 1
             },
-            "color": "#ffffff",
+            "color": "clear",
             "plugin": {
                 "name": "Decision",
                 "config": {
-                    "timeout": 5,
-                    "rightButton": {
-                        "text": "Second option",
-                        "timestamp": 2
-                    },
+                    "timeout": 15,
                     "leftButton": {
-                        "text": "First option",
+                        "text": "Show terrestial planets",
                         "timestamp": "now"
+                    },
+                    "rightButton": {
+                        "text": "Show jovian planets",
+                        "timestamp": 112
                     }
                 }
             }
@@ -234,7 +217,7 @@ const imageConfig = {
             "plugin": {
                 "name": "SimpleLabel",
                 "config": {
-                    "text": "Hi! I'm a label! <a href=\"https://www.google.es\"> Also anchor links</a>"
+                    "text": "Hi! I'm a label!"
                 }
             }
         },
